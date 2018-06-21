7ff3c2def4e2926eb69a83ec1d9423061c12ad6b
Berend Weel <b.weel@esciencecenter.nl>
2018-06-21 10:27:39 +0200

    Added coverage for codacy reporting. Added tests for pushing to docker hub

---
+++ test/test_push.py
@@ -0,0 +1,43 @@
+"""
+    Boatswain test files
+"""
+import pytest
+from boatswain import Boatswain
+
+
+@pytest.mark.usefixtures("ensure_built")
+def test_push(bsfile):
+    """
+        Test creating the Boatswain object
+    """
+    # Should not throw an exception
+    with Boatswain(bsfile) as bosun:  # NOQA
+        pushed = bosun.push()
+
+        assert sorted(pushed, key=str.lower) \
+            == sorted(["image4:pytest", "image3:pytest", "image2:pytest",
+                       "image1:pytest"], key=str.lower)
+
+
+@pytest.mark.usefixtures("ensure_built")
+def test_push_dict(bsfile):
+    """
+        Push only one image with its dependencies
+    """
+    with Boatswain(bsfile) as bosun:
+        pushed = bosun.push_dict(bsfile['images'])
+        assert sorted(pushed, key=str.lower) \
+            == sorted(["image4:pytest", "image3:pytest", "image2:pytest",
+                       "image1:pytest"], key=str.lower)
+
+
+@pytest.mark.usefixtures("ensure_built")
+def test_push_up_to(bsfile):
+    """
+        Push only one image with its dependencies
+    """
+    with Boatswain(bsfile) as bosun:
+        pushed = bosun.push_up_to("image3:pytest", dryrun=True)
+        assert sorted(pushed, key=str.lower) \
+            == sorted(["image3:pytest", "image2:pytest",
+                       "image1:pytest"], key=str.lower)
